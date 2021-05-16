f3209fc4de38584d1f52ea0aedd61885dabb6e1d
SazonovaVV <valeriesazonova@yandex.ru>
2021-05-16 20:03:01 +0300

    модуль для функции minmax

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include<conio.h>
 #include <ctime>
+#include "histogram.h"
 #pragma hdrstop
 using namespace std;
 
@@ -15,23 +16,6 @@ input_numbers(size_t count)
         cin >> result[i];
     }
     return result;
-}
-void
-find_minmax(vector<double> numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (double x : numbers)
-    {
-        if (x < min)
-        {
-            min = x;
-        }
-        else if (x > max)
-        {
-            max = x;
-        }
-    }
 }
 vector <size_t>
 make_histogram(vector<double> numbers, double bin_count,double number_count)
